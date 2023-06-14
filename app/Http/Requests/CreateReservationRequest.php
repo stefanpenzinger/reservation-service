47a1734eb13693bf07409ef9137b7208797636f2
Stefan Penzinger <stefan.penzinger13@gmail.com>
2023-06-14 21:52:49 +0200

    ADDED CreateReservationRequest

---
+++ app/Http/Requests/CreateReservationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateReservationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'integer'],
+            'status' => ['required', 'string', Rule::exists('reservation_status', 'status')],
+            'start_time' => ['required', 'date_format:Y-m-d H:i:s'],
+            'end_time' => ['required', 'date_format:Y-m-d H:i:s', 'after_or_equal:start'],
+        ];
+    }
+}
